4a4e38b47a6a98fd2c86ce947f289ac631995262
Ekaterina <catherinenekrylova@yandex.ry>
2023-01-22 10:49:04 +0200

    Защита

--- gulpfile.js
+++ gulpfile.js
@@ -82,7 +82,7 @@ const copy = (done) => {
   gulp.src([
     'source/fonts/**/*.{woff2,woff}',
     'source/*.ico',
-    'source/*site.webmanifest'
+    'source/manifest.webmanifest'
   ], {
     base: 'source'
   })
