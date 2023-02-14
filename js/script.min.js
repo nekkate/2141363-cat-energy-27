8efb7bb94217e50d30aae3b5e548697a6e4a54f7
nekkate <107505576+nekkate@users.noreply.github.com>
2023-02-14 00:20:52 +0000

    Deploying to gh-pages from @ nekkate/2141363-cat-energy-27@3d03af0e675bd942fb4cff5326fcb545362461d6 🚀

---
+++ js/script.min.js
@@ -0,0 +1,1 @@
+const header=document.querySelector(".main-header"),navToggle=document.querySelector(".main-nav__toggle");header.classList.remove("no-js"),navToggle.addEventListener("click",(()=>header.classList.toggle("is-open")));
