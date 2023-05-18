309f54447749700528b805d1839d3142cef23c1c
nekkate <catherinenekrylova@yandex.ru>
2023-05-18 15:27:44 +0400

    Fix

--- build/js/script.min.js
+++
@@ -1,1 +0,0 @@
-const header=document.querySelector(".main-header"),navToggle=document.querySelector(".main-nav__toggle");header.classList.remove("no-js"),navToggle.addEventListener("click",(()=>header.classList.toggle("is-open")));
